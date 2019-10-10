4a3344574f32310f63387eb726913f42a20b5fd4
Yue Zuo <woshizuoyue9201@yahoo.com>
2019-10-09 21:30:36 -0400

    edit controlSearch

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,42 @@
 
 import Search from './models/Search';
 
-const search = new Search('cookies');
+/**
+ * Global state of the app
+ * - search object
+ * - current recipe object
+ * - shopping list object
+ * - Liked recipes
+ */
 
-console.log(search);
+ const state = {};
 
-search.getResult();
+ const controlSearch = async () =>{
+     // 1. get the query form view
+     const query = 'cookies' // todo
+
+     if(query){
+
+        // 2. new search object and add to state;
+
+        state.search = new Search(query);
+
+        // 3. prepare UI for results
+
+
+        // 4. search for recipes
+
+        await state.search.getResult();
+
+        // 5. render results on UI
+
+        console.log(state.search.result);
+
+     }
+ }
+
+ document.querySelector('.search').addEventListener('submit', e => {
+     
+    e.preventDefault();
+    controlSearch();
+ });
